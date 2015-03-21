e88aa12a6a4400fa77c2ece913b00db97876027e
Neil Pankey <npankey@gmail.com>
2015-03-20 21:24:41 -0700

    Mucking with raw mode

--- Example/main.swift
+++ Example/main.swift
@@ -6,4 +6,15 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 //
 
+import Termios
+import Darwin.C.stdio
+
 println("Hello, World!")
+
+let raw = Termios()
+
+let c = getchar()
+println("\(c)\r")
+print("at start?")
+
+getchar()

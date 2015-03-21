8bece4e3a2d71dbd5f671ad2d26e704845d2016d
Neil Pankey <npankey@gmail.com>
2015-03-21 01:25:18 -0700

    InputFlags values

--- Termios/InputFlags.swift
+++ Termios/InputFlags.swift
@@ -27,4 +27,19 @@ public struct InputFlags : RawOptionSetType {
     public static var allZeros: InputFlags {
         return self(0)
     }
+
+    public static var IGNBRK  : InputFlags { return self(Darwin.IGNBRK)  }
+    public static var BRKINT  : InputFlags { return self(Darwin.BRKINT)  }
+    public static var IGNPAR  : InputFlags { return self(Darwin.IGNPAR)  }
+    public static var PARMRK  : InputFlags { return self(Darwin.PARMRK)  }
+    public static var INPCK   : InputFlags { return self(Darwin.INPCK)   }
+    public static var ISTRIP  : InputFlags { return self(Darwin.ISTRIP)  }
+    public static var INLCR   : InputFlags { return self(Darwin.INLCR)   }
+    public static var IGNCR   : InputFlags { return self(Darwin.IGNCR)   }
+    public static var ICRNL   : InputFlags { return self(Darwin.ICRNL)   }
+    public static var IXON    : InputFlags { return self(Darwin.IXON)    }
+    public static var IXOFF   : InputFlags { return self(Darwin.IXOFF)   }
+    public static var IXANY   : InputFlags { return self(Darwin.IXANY)   }
+    public static var IMAXBEL : InputFlags { return self(Darwin.IMAXBEL) }
+    public static var IUTF8   : InputFlags { return self(Darwin.IUTF8)   }
 }

4d5938797e4a24231865a63adcc5c3bdc5c96029
Neil Pankey <npankey@gmail.com>
2015-03-20 20:01:05 -0700

    Xcode project

---
+++ Termios/Termios.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for Termios.
+extern double TermiosVersionNumber;
+
+/// Project version string for Termios.
+extern const unsigned char TermiosVersionString[];
+

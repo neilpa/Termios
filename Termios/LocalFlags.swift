9230efc627550dad890dbe582e99f7e555fb7829
Neil Pankey <npankey@gmail.com>
2015-03-21 00:56:17 -0700

    Stub out flags

--- Termios/LocalFlags.swift
+++ Termios/LocalFlags.swift
@@ -7,3 +7,34 @@
 //
 
 import Darwin.POSIX.termios
+
+/// Local flag values in a `termios` structure.
+public struct LocalFlags : RawOptionSetType {
+    private var value: UInt = 0
+
+    public init(_ value: UInt) {
+        self.value = value
+    }
+
+    // MARK: RawRepresentable
+
+    public init(rawValue value: UInt) {
+        self.value = value
+    }
+
+    public var rawValue: UInt {
+        return value
+    }
+
+    // MARK: BitwiseOperationsType
+
+    public static var allZeros: LocalFlags {
+        return self(0)
+    }
+
+    // MARK: NilLiteralConvertible
+
+    public init(nilLiteral: ()) {
+        self.value = 0
+    }
+}
--- Termios/OutputFlags.swift
+++ Termios/OutputFlags.swift
@@ -8,3 +8,33 @@
 
 import Darwin.POSIX.termios
 
+/// Output flag values in a `termios` structure.
+public struct OutputFlags : RawOptionSetType {
+    private var value: UInt = 0
+
+    public init(_ value: UInt) {
+        self.value = value
+    }
+
+    // MARK: RawRepresentable
+
+    public init(rawValue value: UInt) {
+        self.value = value
+    }
+
+    public var rawValue: UInt {
+        return value
+    }
+
+    // MARK: BitwiseOperationsType
+
+    public static var allZeros: OutputFlags {
+        return self(0)
+    }
+
+    // MARK: NilLiteralConvertible
+
+    public init(nilLiteral: ()) {
+        self.value = 0
+    }
+}

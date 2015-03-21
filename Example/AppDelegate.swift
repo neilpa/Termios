44bb3d2bd4e820ca52684aec4af3a26dee15965b
Neil Pankey <npankey@gmail.com>
2015-03-20 20:30:17 -0700

    Example target

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Neil Pankey on 3/20/15.
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
